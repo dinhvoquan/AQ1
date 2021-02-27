624d4030c627299f7f2da62784a82728ccfaaa62
QUANPC\Quan <dinhvoquan@gmail.com>
2021-02-28 05:52:55 +0700

    Add filter status and format data in list

---
+++ AQ1.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,18 @@
+﻿(function (app) {
+    //app.filter('statusFilter', function () {
+    //    return function (input) {
+    //        if (input == true)
+    //            return 'Kích hoạt';
+    //        else
+    //            return 'Khóa';
+    //    }
+    //});
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('aq1.common'));
