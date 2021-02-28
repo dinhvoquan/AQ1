738eb36032157c46c0277df333922e457af100fe
QUANPC\Quan <dinhvoquan@gmail.com>
2021-02-28 17:28:40 +0700

    Paging for list

--- AQ1.Web/app/shared/filters/statusFilter.js
+++ AQ1.Web/app/shared/filters/statusFilter.js
@@ -1,12 +1,4 @@
 ﻿(function (app) {
-    //app.filter('statusFilter', function () {
-    //    return function (input) {
-    //        if (input == true)
-    //            return 'Kích hoạt';
-    //        else
-    //            return 'Khóa';
-    //    }
-    //});
     app.filter('statusFilter', function () {
         return function (input) {
             if (input == true)
